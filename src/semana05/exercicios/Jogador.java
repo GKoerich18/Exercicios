bdb4deae05015495c750e8117b32507fa446c062
GKoerich18 <gabrielkoerich2014@outlook.com>
2024-02-05 22:01:27 -0300

    exercicio1

---
+++ src/semana05/exercicios/Jogador.java
@@ -0,0 +1,18 @@
+package semana05.exercicios;
+
+public class Jogador {
+    String nome;
+    int idade;
+    int pontuacao;
+    int numeroTentativas;
+
+    public void adicionaPontos(){
+
+    }
+    public void perdePontos(){
+
+    }
+    public void adicionaTentativa(){
+        
+    }
+}
