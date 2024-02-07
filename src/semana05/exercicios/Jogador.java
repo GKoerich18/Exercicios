feae7cb8cb096c66362f50f0555b9b6536c8d070
GKoerich18 <gabrielkoerich2014@outlook.com>
2024-02-06 22:07:30 -0300

    exercicio02

--- src/semana05/exercicios/Jogador.java
+++ src/semana05/exercicios/Jogador.java
@@ -1,18 +1,67 @@
 package semana05.exercicios;
 
 public class Jogador {
-    String nome;
-    int idade;
-    int pontuacao;
-    int numeroTentativas;
+    private String nome;
+    private int idade;
+    private int pontuacao;
+    private int numeroTentativas;
 
-    public void adicionaPontos(){
+    public Jogador(
+            String nome,
+
+            int idade,
+
+            int pontuacao,
+
+            int numeroTentativas
+
+    ){
+        nome = this.nome;
+        idade = this.idade;
+        pontuacao = this.pontuacao;
+        numeroTentativas = this.numeroTentativas;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public int getNumeroTentativas() {
+        return numeroTentativas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public void setNumeroTentativas(int numeroTentativas) {
+        this.numeroTentativas = numeroTentativas;
+    }
+
+    public void adicionaPontos() {
 
     }
-    public void perdePontos(){
+
+    public void perdePontos() {
 
     }
-    public void adicionaTentativa(){
-        
+
+    public void adicionaTentativa() {
+
     }
 }
