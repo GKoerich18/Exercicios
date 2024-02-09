ebe7ec7abc3a7d85b2fee79aba18c80d7ab85549
GKoerich18 <gabrielkoerich2014@outlook.com>
2024-02-09 19:14:33 -0300

    exercicio03, 04 e 05

--- src/semana05/exercicios/Jogador.java
+++ src/semana05/exercicios/Jogador.java
@@ -1,10 +1,21 @@
 package semana05.exercicios;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Jogador {
+
+//Exercicio 03
+    private ArrayList<Jogador> rankingJogadores;
     private String nome;
     private int idade;
     private int pontuacao;
     private int numeroTentativas;
+
+    public Jogador() {
+        this.rankingJogadores = new ArrayList<>();
+
+    }
 
     public Jogador(
             String nome,
@@ -15,17 +26,59 @@ public class Jogador {
 
             int numeroTentativas
 
-    ){
+    ) {
         nome = this.nome;
         idade = this.idade;
         pontuacao = this.pontuacao;
         numeroTentativas = this.numeroTentativas;
     }
 
-    public String getNome(){
+//Exercicio 05
+    public void novoJogador(Scanner entrada) {
+        do {
+            System.out.print("Digite o nome do novo jogador: ");
+            String nome = entrada.nextLine();
+
+            if (nomeExiste(nome)){
+                System.out.println("Esse nome já foi utilizado, por favor tente outro: ");
+                novoJogador(entrada);
+            }else {
+            Jogador novoJogador = new Jogador();
+            novoJogador.setNome(nome);
+            rankingJogadores.add(novoJogador);
+            }
+
+        }while (nomeExiste(nome));
+
+    }
+
+    private boolean nomeExiste(String nome) {
+        for (Jogador jogador : rankingJogadores) {
+            if (jogador.getNome().equalsIgnoreCase(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void validarJogador(Scanner entrada){
+        novoJogador(entrada);
+    }
+
+    //Exercicio 04
+    public void exibirTop10() {
+        System.out.println("Lista de Jogadores:");
+        for (int i = 0; i < rankingJogadores.size() && i <10; i++) {
+            Jogador jogador = rankingJogadores.get(i);
+            System.out.println(i  + " - " + jogador.getNome());
+        }
+    }
+
+    public String getNome() {
         return nome;
     }
-    public int getIdade(){
+
+    public int getIdade() {
         return idade;
     }
 
@@ -64,4 +117,6 @@ public class Jogador {
     public void adicionaTentativa() {
 
     }
+
+
 }
